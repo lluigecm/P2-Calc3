0cde5dae8ec7cdd60b53bd124aa4c501808bdf49
Luige <luige030301@gmail.com>
2024-11-01 20:42:51 -0300

    I'll have to redo all the code to solve the questions for any value -_-

--- src/app.py
+++ src/app.py
@@ -2,10 +2,6 @@ import PySimpleGUI as sg
 from PIL import Image
 import ctypes
 
-from GT1 import GT1
-from GT2 import GT2
-from GT3 import GT3
-
 sg.theme('DarkBlue3')
 
 
@@ -14,11 +10,7 @@ class App:
     def __init__(self):
         self.selected_gt = None
 
-        self.layout = [
-            [sg.Text('Selecione o GT para visualizar as tarefas:')],
-            [sg.Button('GT1'), sg.Button('GT2'), sg.Button('GT3')],
-            [sg.Image('', key='-IMG-')],
-            [sg.Button('', key='-RESOLUCAO-', visible=False)]
+        self.layout = [[]
         ]
 
         self.window = sg.Window('Resolução Prova 2', self.layout, size=(500, 100), element_justification='center')
@@ -39,8 +31,6 @@ class App:
 
         self.window.TKroot.geometry(f'{width}x{height + 100}+{new_x}+{new_y}')
 
-        # Update the "Resolução" button text and make it visible
-        self.window['-RESOLUCAO-'].update(text=f'Resolução {gt}', visible=True)
 
     def run(self):
         while True:
@@ -48,24 +38,5 @@ class App:
 
             if event == sg.WIN_CLOSED:
                 break
-
-            if event == 'GT1':
-                self.update_image("./imgs/gt1.png", 'GT1')
-
-            if event == 'GT2':
-                self.update_image("./imgs/gt2.png", 'GT2')
-
-            if event == 'GT3':
-                self.update_image("./imgs/gt3.png", 'GT3')
-
-            if event == '-RESOLUCAO-':
-                if self.selected_gt == 'GT1':
-                    GT1()
-
-                if self.selected_gt == 'GT2':
-                    GT2()
-
-                if self.selected_gt == 'GT3':
-                    GT3()
 
         self.window.close()
